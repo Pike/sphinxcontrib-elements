b595a48540b2866f406a4dd2a9717ad7dc7ffcd0
Axel Hecht <axel@pike.org>
2020-11-05 23:06:46 +0100

    Make demo configs work for the example doc of sphinx-themes v2

--- themes/demos/classic/conf.py
+++ themes/demos/classic/conf.py
@@ -1,3 +1,28 @@
+import os
+import sys
+
+sys.path.append(os.path.join(sys.argv[-2], "demo"))
+print("", sys.path[-1], "", sep="\n" + "-" * 80 + "\n")
+
 html_theme = 'classic'
 html_theme_options = {
 }
+
+# -- Project information -----------------------------------------------------
+
+project = "Sphinx Themes Sample"
+copyright = "2020, Pradyun Gedam"
+author = "Pradyun Gedam"
+language = "en"
+
+# -- Extensions --------------------------------------------------------------
+extensions = [
+    "sphinx.ext.intersphinx",
+    "sphinx.ext.autodoc",
+    "sphinx.ext.mathjax",
+    "sphinx.ext.viewcode",
+]
+
+# -- Options for HTML output -------------------------------------------------
+
+html_title = project
